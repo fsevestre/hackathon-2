c7c489cd4e281ede8985798f61ee76098b092a3f
fsevestre <aniki.taicho@gmail.com>
2016-05-04 16:40:00 +0200

    Fix LevelD

--- LevelD/Brute.php
+++ LevelD/Brute.php
@@ -21,7 +21,41 @@ class Brute
      */
     public function force()
     {
+        if (preg_match('/^[a-f0-9]{32}$/', $this->hash)) { // md5
+            $continue = true;
+            $val = 'aaaa';
+            while ($continue) {
+                if (md5($val) === $this->hash) {
+                    $this->origin = $val;
+                    $continue = false;
+                }
 
-        // @TODO
+                ++$val;
+            }
+        } elseif (is_numeric($this->hash)) { // crc32
+            $continue = true;
+            $val = 'aaaa';
+            while ($continue) {
+                if ((string) crc32($val) === $this->hash) {
+                    $this->origin = $val;
+                    $continue = false;
+                }
+
+                ++$val;
+            }
+        } elseif (preg_match('/^[a-f0-9]{40}$/', $this->hash)) { // md5
+            $continue = true;
+            $val = 'aaaa';
+            while ($continue) {
+                if (sha1($val) === $this->hash) {
+                    $this->origin = $val;
+                    $continue = false;
+                }
+
+                ++$val;
+            }
+        } else {
+            $this->origin = base64_decode($this->hash);
+        }
     }
 }
